108e7b0fb19f03b777dc09057fa26ee6288df592
Subir Ranjan <subir.ranjan1@gmail.com>
2025-06-10 21:47:30 -0700

    Inner join using Using Clause

--- JoinQueries.sql
+++ JoinQueries.sql
@@ -1,3 +1,11 @@
 select * from monarchs;
 
 Select * from pm_president;
+
+-- Inner Join with using clause
+SELECT
+	P1.PRESIDENT,
+	P2.PRIME_MINISTER
+FROM
+	PRESIDENTS AS P1
+	INNER JOIN PRIME_MINISTERS AS P2 USING (COUNTRY)
